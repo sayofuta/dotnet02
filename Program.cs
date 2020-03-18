dbc5a319929df49164f8c2392507be752a2421fb
二村　紗世 <sayofuta@fsi.co.jp>
2020-03-18 09:50:30 +0900

    helloworld

--- Program.cs
+++ Program.cs
@@ -6,14 +6,7 @@ namespace myapp
     {
         static void Main(string[] args)
         {
-            var counter = 0;
-            var max = args.Length != 0 ? Convert.ToInt32(args[0]) : -1;
-            while (max == -1 || counter < 10)
-            {
-                counter++;
-                Console.WriteLine($"Counter: {counter}");
-                System.Threading.Tasks.Task.Delay(1000).Wait();
-            }
+            Console.WriteLine("Hello World!");
         }
     }
 }
